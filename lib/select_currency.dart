cc598fc35ea67d0604ab5e7c3f5dd46e1e826200
chinotapales <carlo.tapales@coreproc.ph>
2018-09-20 12:54:22 +0800

    Fixed Indentation

--- lib/select_currency.dart
+++ lib/select_currency.dart
@@ -53,118 +53,118 @@ class _SelectCurrencyState extends State<SelectCurrencyPage> {
         ),
       ),
       body: new NestedScrollView(
-              headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-                return <Widget>[
-                ];
-              },
-              body: new Column(
-                children: <Widget>[
-                  new Padding(
-                    padding: new EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+          ];
+        },
+        body: new Column(
+          children: <Widget>[
+            new Padding(
+              padding: new EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
+              child: new Container(
+                color: Colors.transparent,
+                child: new Container(
+                  padding: new EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 0.0),
+                  decoration: new BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(1.0, 1.0),
+                        blurRadius: 3.0,
+                      ),
+                    ],
+                  ),
+                  child: new Center(
                     child: new Container(
-                      color: Colors.transparent,
-                      child: new Container(
-                        padding: new EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 0.0),
-                        decoration: new BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
-                          boxShadow: <BoxShadow>[
-                            BoxShadow(
-                              color: Colors.grey,
-                              offset: Offset(1.0, 1.0),
-                              blurRadius: 3.0,
-                            ),
-                          ],
+                      child: new TextField(
+                        decoration: new InputDecoration(
+                          icon: Icon(Icons.search),
+                          hintText: "Search",
+                          border: InputBorder.none,
                         ),
-                        child: new Center(
-                          child: new Container(
-                            child: new TextField(
-                              decoration: new InputDecoration(
-                                icon: Icon(Icons.search),
-                                hintText: "Search",
-                                border: InputBorder.none,
-                              ),
-                              style: new TextStyle(
-                                color: Colors.black,
-                                fontSize: 12.0,
-                                fontFamily: "Futura",
-                              ),
-                              onChanged: (text) {
-                                setState(() {
-                                  _isSearchOpened = true;
-                                });
-                              },
-                              onSubmitted: (text) {
-                                var searchIndices = List();
-                                if (text.isEmpty) {
-                                  searchIndices = this.keyIndices;
-                                }
-                                else {
-                                  searchIndices = keyIndices.where((item) => item.toString().contains(text.toUpperCase())).toList();
-                                }
-                                setState(() {
-                                  this.searchIndices = searchIndices;
-                                  this.rates = this.rates;
-                                  _isSearchOpened = false;
-                                });
-                              },
-                            ),
-                          ),
+                        style: new TextStyle(
+                          color: Colors.black,
+                          fontSize: 12.0,
+                          fontFamily: "Futura",
                         ),
+                        onChanged: (text) {
+                          setState(() {
+                            _isSearchOpened = true;
+                          });
+                        },
+                        onSubmitted: (text) {
+                          var searchIndices = List();
+                          if (text.isEmpty) {
+                            searchIndices = this.keyIndices;
+                          }
+                          else {
+                            searchIndices = keyIndices.where((item) => item.toString().contains(text.toUpperCase())).toList();
+                          }
+                          setState(() {
+                            this.searchIndices = searchIndices;
+                            this.rates = this.rates;
+                            _isSearchOpened = false;
+                          });
+                        },
                       ),
                     ),
                   ),
-                  new Expanded(
-                    flex: 1,
-                    child: new Padding(
-                      padding: new EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
-                      child: new Container(
-                        color: Colors.transparent,
-                        child: new Container(
-                          padding: new EdgeInsets.all(12.0),
-                          decoration: new BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
-                            boxShadow: <BoxShadow>[
-                              BoxShadow(
-                                color: Colors.grey,
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 3.0,
-                              ),
-                            ],
-                          ),
-                          child: new Center(
-                            child: _isSearchOpened ? new Center(
-                              child: new CircularProgressIndicator(),
-                              ) : new ListView.builder(
-                              itemCount: this.rates != null ? this.searchIndices.length : 0,
-                              itemBuilder: (context, index) {
-                                final rate = rates[this.searchIndices[index]];
-                                return new Container(
-                                  height: 42.0,
-                                  child: new Column(
-                                    children: <Widget>[
-                                      new Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: <Widget>[
-                                          new Text(rate["flag"] + " " + searchIndices[index]),
-                                          new Text(rate["symbol"] + rate["value"].toStringAsFixed(2)),
-                                        ],
-                                      ),
-                                      new Divider(),
-                                    ],
-                                  ),
-                                );
-                              }
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
+            new Expanded(
+              flex: 1,
+              child: new Padding(
+                padding: new EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
+                child: new Container(
+                  color: Colors.transparent,
+                  child: new Container(
+                    padding: new EdgeInsets.all(12.0),
+                    decoration: new BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(1.0, 1.0),
+                          blurRadius: 3.0,
+                        ),
+                      ],
+                    ),
+                    child: new Center(
+                      child: _isSearchOpened ? new Center(
+                        child: new CircularProgressIndicator(),
+                        ) : new ListView.builder(
+                        itemCount: this.rates != null ? this.searchIndices.length : 0,
+                        itemBuilder: (context, index) {
+                          final rate = rates[this.searchIndices[index]];
+                          return new Container(
+                            height: 42.0,
+                            child: new Column(
+                              children: <Widget>[
+                                new Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    new Text(rate["flag"] + " " + searchIndices[index]),
+                                    new Text(rate["symbol"] + rate["value"].toStringAsFixed(2)),
+                                  ],
+                                ),
+                                new Divider(),
+                              ],
+                            ),
+                          );
+                        }
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
